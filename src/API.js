1567d2de93690e810b57136e5d07b250f8a39982
MarcosPerez16 <marcos.perez06@yahoo.com>
2024-03-24 06:51:28 -0700

    created functions in API.js for login, getAllProducts, and getSingleProduct

---
+++ src/API.js
@@ -0,0 +1,47 @@
+const APIURL = "https://fakestoreapi.com";
+
+//User login function
+export const login = async (username, password) => {
+  try {
+    const response = await fetch(`${APIURL}/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+
+    const result = await response.json();
+
+    return result.token;
+  } catch (error) {
+    console.error("Error trying to login", error);
+  }
+};
+
+//GET all products
+
+export const getAllProducts = async () => {
+  try {
+    const response = await fetch(`${APIURL}/products`);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error getting all products", error);
+  }
+};
+
+//GET single product
+
+export const getSingleProduct = async (id) => {
+  try {
+    const response = await fetch(`${APIURL}/products/${id}`);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error("Error getting single product", error);
+  }
+};
